b264f7fa12d6a8005f78b9583b20f0752e8e3ce3
Maxi <herreramaxi@gmail.com>
2022-01-08 23:49:46 +0000

    adding some styling and validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,16 @@ import { AppComponent } from './app.component';
 import { BankLoanCalculatorComponent } from './bank-loan-calculator/bank-loan-calculator.component';
 import { BaseUrlInterceptor } from './BaseUrlInterceptor';
 import { environment } from 'src/environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -13,7 +23,17 @@ import { environment } from 'src/environments/environment';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatToolbarModule,
+    NgbModule
   ],
   providers: [
     { provide: 'BASE_API_URL', useValue: environment.baseUrl },
--- src/app/bank-loan-calculator/bank-loan-calculator.component.ts
+++ src/app/bank-loan-calculator/bank-loan-calculator.component.ts
@@ -1,24 +1,59 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-bank-loan-calculator',
   templateUrl: './bank-loan-calculator.component.html',
   styleUrls: ['./bank-loan-calculator.component.css']
 })
+
 export class BankLoanCalculatorComponent implements OnInit {
 
-  constructor(private http: HttpClient,) { }
+  formGroup: FormGroup;
+  monthlyPayment: string | undefined;
+  totalRepayment: string | undefined;
+
+  constructor(private http: HttpClient, private fb: FormBuilder) {
+    this.formGroup = this.fb.group({
+      amount: ['', [Validators.required]],
+      anualInterestRate: ['', [Validators.required, Validators.min(0.00001), Validators.max(100)]],
+      periodInMonths: ['', [Validators.required, Validators.min(1), Validators.max(480)]]
+    });
+  }
 
   ngOnInit(): void {
   }
 
-  onClick() {
-    console.log("onClick")
-    var url = "api/totalrepayment/599.550/360";
-    this.http.get(url).subscribe(r => {
-      console.log(r)
+  onSubmit() {
+    if (!this.formGroup || !this.formGroup.valid) return;
 
+    var amount = this.formGroup.get('amount')?.value;
+    var anualInterestRate = this.formGroup.get('anualInterestRate')?.value;
+    var periodInMonths = this.formGroup.get('periodInMonths')?.value;
+
+    var url = `api/monthlypayment/${anualInterestRate}/${periodInMonths}/${amount}`;
+    this.http.get(url).subscribe(r => {
+      if (!r) return;
+
+      const monthlyPaymentValue = parseFloat(r.toString());
+      this.monthlyPayment = monthlyPaymentValue.toFixed(2);
+
+      this.http.get(`api/totalrepayment/${monthlyPaymentValue}/${periodInMonths}`).subscribe(s => {
+        if (!s) return;
+        this.totalRepayment = parseFloat(s.toString()).toFixed(2);
+      });
     })
+  }
+
+  clearControls() {
+    this.monthlyPayment = undefined;
+    this.totalRepayment = undefined;
+    this.formGroup.reset();
+  }
+
+  isValidInput(fieldName: any): boolean {
+    return this.formGroup.controls[fieldName].invalid &&
+      (this.formGroup.controls[fieldName].dirty || this.formGroup.controls[fieldName].touched);
   }
 }
